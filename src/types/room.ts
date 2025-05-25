d608ee4fa8ecb7aca4d02f2c82ec33627e128847
greenwinther <dennisgw@live.se>
2025-05-25 22:41:15 +0200

    feat: add Room, Player, and Song type definitions

---
+++ src/types/room.ts
@@ -0,0 +1,21 @@
+export type Player = {
+	id: number;
+	name: string;
+	roomId: number;
+};
+
+export type Song = {
+	id: number;
+	url: string;
+	submitter: string;
+	roomId: number;
+};
+
+export type Room = {
+	id: number;
+	code: string;
+	theme: string;
+	backgroundUrl?: string | null;
+	players: Player[];
+	songs: Song[];
+};
