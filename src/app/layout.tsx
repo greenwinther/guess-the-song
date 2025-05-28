27959e96ec0f20674ba953506b35d204d9c31a2f
greenwinther <dennisgw@live.se>
2025-05-28 14:57:58 +0200

    feat: import global styles in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { GameProvider } from "@/contexts/GameContext";
 import { SocketProvider } from "@/contexts/SocketContext";
+import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
